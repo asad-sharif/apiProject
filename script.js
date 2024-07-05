89161f92c5da657d940ba5afa33b49ac3a368453
asad-sharif <asadsharif1270@gmail.com>
2024-07-05 16:56:11 +0500

    random advice generator

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+const btn = document.querySelector('#btn');
+const adviceContainer = document.querySelector('#advice');
+
+btn.addEventListener('click', async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    console.log(data.slip.advice);
+    
+    let advice = document.createElement('p');
+    advice.innerText = data.slip.advice;
+    
+    // Clear previous advice
+    adviceContainer.innerHTML = '';
+    
+    adviceContainer.appendChild(advice);
+});
